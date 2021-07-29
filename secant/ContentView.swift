52d3b8610a122c0ac981624b724ff1fc3d0f8f21
Francisco Gindre <francisco.gindre@gmail.com>
2021-07-29 19:38:35 -0300

    Initial Project

---
+++ secant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  secant
+//
+//  Created by Francisco Gindre on 7/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Zcash!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ secant/SecantApp.swift
@@ -0,0 +1,17 @@
+//
+//  secantApp.swift
+//  secant
+//
+//  Created by Francisco Gindre on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SecantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
