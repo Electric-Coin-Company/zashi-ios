033d568d86f6147d44b335908ed10dba4542383c
Francisco Gindre <francisco.gindre@gmail.com>
2021-12-30 15:02:55 -0300

    Accessibility Pass on Validation success screen

--- secant/Features/BackupFlow/Views/ValidationSuccededView.swift
+++ secant/Features/BackupFlow/Views/ValidationSuccededView.swift
@@ -10,32 +10,37 @@ import ComposableArchitecture
 
 struct ValidationSuccededView: View {
     var store: RecoveryPhraseValidationStore
-
+    @ScaledMetric var scaledPadding: CGFloat = 10
+    @ScaledMetric var scaledButtonHeight: CGFloat = 130
     var body: some View {
         WithViewStore(store) { viewStore in
             GeometryReader { proxy in
                 VStack {
-                    VStack(spacing: 10) {
+                    VStack(spacing: 20) {
                         Text("Success!")
                             .font(.custom(FontFamily.Rubik.regular.name, size: 36))
 
                         Text("Place that backup somewhere safe and venture forth in security.")
-                            .font(.custom(FontFamily.Rubik.regular.name, size: 17))
+                            .bodyText()
                             .multilineTextAlignment(.center)
                             .lineSpacing(2)
-                            .frame(width: proxy.size.width * 0.7)
+                            .fixedSize(horizontal: false, vertical: true)
                     }
-                    .padding(.bottom, 75)
 
-                    CircularFrame()
-                        .backgroundImage(
-                            Asset.Assets.Backgrounds.callout1.image
-                        )
-                        .frame(
-                            width: proxy.size.width * 0.84,
-                            height: proxy.size.width * 0.84
-                        )
-                        .badgeIcon(.shield)
+                    Spacer()
+
+                    VStack {
+                        CircularFrame()
+                            .backgroundImage(
+                                Asset.Assets.Backgrounds.callout1.image
+                            )
+                            .frame(
+                                width: proxy.size.width * 0.84,
+                                height: proxy.size.width * 0.84
+                            )
+                            .badgeIcon(.shield)
+                    }
+                    .padding(.vertical, 20)
 
                     Spacer()
 
@@ -44,36 +49,52 @@ struct ValidationSuccededView: View {
                             action: {
                                 viewStore.send(.proceedToHome, animation: .easeIn(duration: 1))
                             },
-                            label: { Text("Take me to my wallet!") }
+                            label: {
+                                Text("Take me to my wallet!")
+                                    .fixedSize(horizontal: false, vertical: true)
+                            }
                         )
                         .activeButtonStyle
                         .frame(
-                            width: proxy.size.width * 0.8,
-                            height: 60
+                            minHeight: 60,
+                            idealHeight: 60,
+                            maxHeight: .infinity
                         )
 
                         Button(
                             action: { viewStore.send(.displayBackedUpPhrase, animation: .easeIn(duration: 1)) },
-                            label: { Text("Show me my phrase again") }
+                            label: {
+                                Text("Show me my phrase again")
+                                    .fixedSize(horizontal: false, vertical: true)
+
+                            }
                         )
                         .secondaryButtonStyle
                         .frame(
-                            width: proxy.size.width * 0.8,
-                            height: 60
+                            minHeight: 60,
+                            idealHeight: 60,
+                            maxHeight: .infinity
                         )
                     }
+                    .frame(height: scaledButtonHeight)
+                    .padding(.vertical, scaledPadding)
                 }
-                .frame(width: proxy.size.width)
-                .padding(.vertical, 20)
-                .applyScreenBackground()
+
+                .padding(.horizontal)
+                .scrollableWhenScaledUp()
             }
         }
         .navigationBarBackButtonHidden(true)
+        .applyScreenBackground()
     }
 }
 
 struct ValidationSuccededView_Previews: PreviewProvider {
     static var previews: some View {
-        ValidationSuccededView(store: RecoveryPhraseValidationStore.demo)
+        Group {
+            ValidationSuccededView(store: RecoveryPhraseValidationStore.demo)
+            ValidationSuccededView(store: RecoveryPhraseValidationStore.demo)
+                .environment(\.sizeCategory, .accessibilityExtraLarge)
+        }
     }
 }
