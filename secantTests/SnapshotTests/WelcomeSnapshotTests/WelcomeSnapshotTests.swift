4a2ff22734ab5e0d0060b81cd625aa81d976f9de
Lukas Korba <lukas.korba@seznam.cz>
2022-06-07 12:20:35 +0200

    [337] Set up Snapshot Testing
    - snapshot package added
    - welcome snapshot taken
    
    [337] Set up Snapshot Testing
    
    - iPhone 8 Plus welcome screen references
    
    [337] Set up Snapshot Testing
    
    - snapshot references removed
    - snapshots only recorded, no comparison
    
    [337] Set up Snapshot Testing
    
    - isRecording removed
    
    [337] Set up Snapshot Testing
    
    - iPhone 8 Plus, iOS 15.4 screenshots
    
    [337] Set up Snapshot Testing
    
    - lowering the precision to 0.999
    
    [337] Set up Snapshot Testing
    
    - attempts to get it pass the test
    
    [337] Set up Snapshot Testing
    
    - frame set up
    
    [337] Set up Snapshot Testing
    
    - resized image
    
    [337] Set up Snapshot Testing
    
    - many different snapshot tests
    
    [337] Set up Snapshot Testing
    
    - low precision test
    
    [337] Set up Snapshot Testing
    
    - updated pngs
    
    [337] Set up Snapshot Testing
    
    - another set of pngs
    
    [337] Set up Snapshot Testing
    
    - precision 0.99
    
    [337] Set up Snapshot Testing
    
    - XCTAttachments
    
    [337] Set up Snapshot Testing
    
    - typo fixed
    
    [337] Set up Snapshot Testing
    
    - reference images
    
    [337] Set up Snapshot Testing
    
    - no precision set
    
    [337] Set up Snapshot Testing
    
    - screenshot names added
    - precision so low it passes all the time
    
    [337] Set up Snapshot Testing (350)
    
    - cleanup
    
    [337] Set up Snapshot Testing (350)
    
    - removed snapshot library completely
    
    [337] Set up Snapshot Testing (350)
    
    - code simplification and cleanup
    
    [337] Set up Snapshot Testing (350)
    
    - adding attachments refactored to simplify the code even more
    
    [337] Set up Snapshot Testing (350)
    
    - last simplification to wrap both light and dark under the hood
    
    [337] Set up Snapshot Testing (350)
    
    - addAttachments method to follow the same XCTestCase terminology
    
    [337] Set up Snapshot Testing (350)
    
    - attachments name fix

---
+++ secantTests/SnapshotTests/WelcomeSnapshotTests/WelcomeSnapshotTests.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeSnapshotTests.swift
+//  secantTests
+//
+//  Created by Lukáš Korba on 06.06.2022.
+//
+
+import XCTest
+@testable import secant_testnet
+import ComposableArchitecture
+
+class WelcomeSnapshotTests: XCTestCase {
+    func testWelcomeSnapshot() throws {
+        let store = Store(
+            initialState: .placeholder,
+            reducer: WelcomeReducer.default,
+            environment: WelcomeEnvironment()
+        )
+
+        addAttachments(WelcomeView(store: store))
+    }
+}
