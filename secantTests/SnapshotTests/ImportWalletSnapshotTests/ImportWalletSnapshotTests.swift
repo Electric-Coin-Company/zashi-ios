5e082ce5b50a0e5b42d07a5c53024e77dd714298
Lukas Korba <lukas.korba@seznam.cz>
2022-06-13 12:24:00 +0200

    [345] Take wallet import snapshot
    - snapshot of import wallet, demo environment

---
+++ secantTests/SnapshotTests/ImportWalletSnapshotTests/ImportWalletSnapshotTests.swift
@@ -0,0 +1,22 @@
+//
+//  ImportWalletSnapshotTests.swift
+//  secantTests
+//
+//  Created by Lukáš Korba on 13.06.2022.
+//
+
+import XCTest
+@testable import secant_testnet
+import ComposableArchitecture
+
+class ImportWalletSnapshotTests: XCTestCase {
+    func testImportWalletSnapshot() throws {
+        let store = ImportWalletStore(
+            initialState: .placeholder,
+            reducer: .default,
+            environment: .demo
+        )
+        
+        addAttachments(ImportWalletView(store: store))
+    }
+}
