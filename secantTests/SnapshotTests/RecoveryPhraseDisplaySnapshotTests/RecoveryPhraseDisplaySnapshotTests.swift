7625fd5a1956136134732762e9372eaddffe314b
Lukas Korba <lukas.korba@seznam.cz>
2022-06-13 12:35:42 +0200

    [341] Take Phrase Display Snapshots
    - phrase display with mocked data, .demo environment

---
+++ secantTests/SnapshotTests/RecoveryPhraseDisplaySnapshotTests/RecoveryPhraseDisplaySnapshotTests.swift
@@ -0,0 +1,22 @@
+//
+//  RecoveryPhraseDisplaySnapshotTests.swift
+//  secantTests
+//
+//  Created by Lukáš Korba on 13.06.2022.
+//
+
+import XCTest
+@testable import secant_testnet
+import ComposableArchitecture
+
+class RecoveryPhraseDisplaySnapshotTests: XCTestCase {
+    func testRecoveryPhraseDisplaySnapshot() throws {
+        let store = RecoveryPhraseDisplayStore(
+            initialState: .init(phrase: .placeholder),
+            reducer: .default,
+            environment: .demo
+        )
+        
+        addAttachments(RecoveryPhraseDisplayView(store: store))
+    }
+}
