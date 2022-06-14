edeb3f7fee29d8edee8f9f14717a21ac5f9e6d08
Lukas Korba <LukasKorba@users.noreply.github.com>
2022-06-14 11:51:13 +0200

    [340] Take Onboarding Snapshots (#352)
    - the OnboardingFlow info steps snapshots
    
    [340] Take Onboarding Snapshots (352)
    
    - cleanup

---
+++ secantTests/SnapshotTests/OnboardingSnapshotTests/OnboardingSnapshotTests.swift
@@ -0,0 +1,48 @@
+//
+//  OnboardingSnapshotTests.swift
+//  secantTests
+//
+//  Created by Lukáš Korba on 13.06.2022.
+//
+
+import XCTest
+@testable import secant_testnet
+import ComposableArchitecture
+
+class OnboardingSnapshotTests: XCTestCase {
+    func testOnboardingFlowSnapshot() throws {
+        let store = OnboardingFlowStore(
+            initialState: OnboardingFlowState(importWalletState: .placeholder),
+            reducer: .default,
+            environment: .demo
+        )
+        let viewStore = ViewStore(store)
+
+        // step 1
+        addAttachments(
+            name: "\(#function)_info1",
+            OnboardingScreen(store: store)
+        )
+        
+        // step 2
+        viewStore.send(.next)
+        addAttachments(
+            name: "\(#function)_info2",
+            OnboardingScreen(store: store)
+        )
+
+        // step 3
+        viewStore.send(.next)
+        addAttachments(
+            name: "\(#function)_info3",
+            OnboardingScreen(store: store)
+        )
+
+        // step 4
+        viewStore.send(.next)
+        addAttachments(
+            name: "\(#function)_info4",
+            OnboardingScreen(store: store)
+        )
+    }
+}
