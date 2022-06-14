05b2d716b16194c24ebf41c43431504ba6a45324
Lukas Korba <LukasKorba@users.noreply.github.com>
2022-06-14 12:00:08 +0200

    [342] Take empty validation puzzle snapshot (#355)
    - the backup is the whole flow so the implementation covers all screens:
    - preamble
    - validation with 4 steps to fill in the chips
    - success/failure screens

---
+++ secantTests/SnapshotTests/RecoveryPhraseValidationFlowSnapshotTests/RecoveryPhraseValidationFlowSnapshotTests.swift
@@ -0,0 +1,86 @@
+//
+//  RecoveryPhraseValidationFlowSnapshotTests.swift
+//  secantTests
+//
+//  Created by Lukáš Korba on 13.06.2022.
+//
+
+import XCTest
+@testable import secant_testnet
+import ComposableArchitecture
+
+// swiftlint:disable:next type_name
+class RecoveryPhraseValidationFlowSnapshotTests: XCTestCase {
+    func testRecoveryPhraseValidationFlowPreambleSnapshot() throws {
+        let store = RecoveryPhraseValidationFlowStore(
+            initialState: .placeholder,
+            reducer: .default,
+            environment: .demo
+        )
+
+        addAttachments(RecoveryPhraseValidationFlowView(store: store))
+    }
+
+    func testRecoveryPhraseValidationFlowBackupSnapshot() throws {
+        let store = RecoveryPhraseValidationFlowStore(
+            initialState: .placeholder,
+            reducer: .default,
+            environment: .demo
+        )
+        let viewStore = ViewStore(store)
+
+        // empty
+        addAttachments(
+            name: "\(#function)_empty",
+            RecoveryPhraseBackupView(store: store)
+        )
+        
+        // 1st chip in place
+        viewStore.send(.move(wordChip: .unassigned(word: "thank"), intoGroup: 0))
+        addAttachments(
+            name: "\(#function)_1stChipInPlace",
+            RecoveryPhraseBackupView(store: store)
+        )
+
+        // 2nd chip in place
+        viewStore.send(.move(wordChip: .unassigned(word: "morning"), intoGroup: 1))
+        addAttachments(
+            name: "\(#function)_2ndChipInPlace",
+            RecoveryPhraseBackupView(store: store)
+        )
+
+        // 3rd chip in place
+        viewStore.send(.move(wordChip: .unassigned(word: "boil"), intoGroup: 2))
+        addAttachments(
+            name: "\(#function)_3rdChipInPlace",
+            RecoveryPhraseBackupView(store: store)
+        )
+
+        // 4th chip in place
+        viewStore.send(.move(wordChip: .unassigned(word: "garlic"), intoGroup: 3))
+        addAttachments(
+            name: "\(#function)_4thChipInPlace",
+            RecoveryPhraseBackupView(store: store)
+        )
+    }
+    
+    func testRecoveryPhraseValidationFlowSucceededSnapshot() throws {
+        let store = RecoveryPhraseValidationFlowStore(
+            initialState: .placeholder,
+            reducer: .default,
+            environment: .demo
+        )
+
+        addAttachments(RecoveryPhraseBackupSucceededView(store: store))
+    }
+
+    func testRecoveryPhraseValidationFlowFailedSnapshot() throws {
+        let store = RecoveryPhraseValidationFlowStore(
+            initialState: .placeholder,
+            reducer: .default,
+            environment: .demo
+        )
+
+        addAttachments(RecoveryPhraseBackupFailedView(store: store))
+    }
+}
