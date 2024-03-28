10399c9c6c2a3cf79638bf976ddf052e1696307a
Lukas Korba <lukas.korba@seznam.cz>
2024-03-28 16:17:42 +0100

    [#1158] Release 1.0.4
    - Changelog updated
    - SDK version bump

--- modules/Package.swift
+++ modules/Package.swift
@@ -69,7 +69,7 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.1.0"),
         .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.0"),
         .package(url: "https://github.com/zcash-hackworks/MnemonicSwift", from: "2.2.4"),
-        .package(url: "https://github.com/Electric-Coin-Company/zcash-swift-wallet-sdk", from: "2.1.2"),
+        .package(url: "https://github.com/Electric-Coin-Company/zcash-swift-wallet-sdk", from: "2.1.3"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.17.0")
     ],
     targets: [
