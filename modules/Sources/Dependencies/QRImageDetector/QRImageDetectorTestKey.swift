1cb9ad146592ab7f706cbfeb7478e10d543fb222
Lukas Korba <lukas.korba@seznam.cz>
2024-04-18 09:31:59 +0200

    read-qr-code-from-image
    - The ZashiImagePicker implemented that opens a library and reads an image from it
    - QR code image detector implemented
    
    read-qr-code-from-image
    
    - QRImageDetector dependency implemented
    
    read-qr-code-from-image
    
    - Multiple codes detection + handling. For it's an error because more codes could result in wrong pick from user's point of view.
    
    read-qr-code-from-image
    
    - final design
    
    [#1231] QR code from a photo saved in my library
    
    - changelog updated

---
+++ modules/Sources/Dependencies/QRImageDetector/QRImageDetectorTestKey.swift
@@ -0,0 +1,21 @@
+//
+//  QRImageDetectorTestKey.swift
+//  secant-testnet
+//
+//  Created by Lukáš Korba on 2024-04-18.
+//
+
+import ComposableArchitecture
+import XCTestDynamicOverlay
+
+extension QRImageDetectorClient: TestDependencyKey {
+    public static let testValue = Self(
+        check: XCTUnimplemented("\(Self.self).check", placeholder: nil)
+    )
+}
+
+extension QRImageDetectorClient {
+    public static let noOp = Self(
+        check: { _ in nil }
+    )
+}
