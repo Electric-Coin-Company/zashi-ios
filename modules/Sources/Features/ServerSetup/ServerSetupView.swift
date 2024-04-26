3c6ca1bde23e7df72374f5b27ad8437d13e0484d
Lukas Korba <lukas.korba@seznam.cz>
2024-04-26 14:32:24 +0200

    [#1221] Scroll to selected server
    - Automatically scroll to the selected server so even the one at the bottom of the list that is not visible will be scrolled to the visual area.
    
    [#1221] Scroll to selected server (#1222)
    
    - named constant

--- modules/Sources/Features/ServerSetup/ServerSetupView.swift
+++ modules/Sources/Features/ServerSetup/ServerSetupView.swift
@@ -13,6 +13,15 @@ import UIComponents
 import ZcashSDKEnvironment
 
 public struct ServerSetupView: View {
+    private enum Constants {
+        // Time it takes to initiate the animated scroll after the screen is presented
+        static let delayBeforeScroll = 500_000_000
+    }
+    
+    private enum InputID: Hashable {
+        case selection
+    }
+    
     @Perception.Bindable var store: StoreOf<ServerSetup>
     
     public init(store: StoreOf<ServerSetup>) {
@@ -21,99 +30,116 @@ public struct ServerSetupView: View {
 
     public var body: some View {
         WithPerceptionTracking {
-            VStack(alignment: .center, spacing: 0) {
-                Asset.Colors.secondary.color
-                    .frame(height: 1)
-            
-                ScrollView {
-                    ForEach(ZcashSDKEnvironment.Servers.allCases, id: \.self) { server in
-                        VStack {
-                            HStack(spacing: 0) {
-                                Button {
-                                    store.send(.someServerTapped(server))
-                                } label: {
-                                    HStack(spacing: 10) {
-                                        WithPerceptionTracking {
-                                            if server == store.server {
-                                                Circle()
-                                                    .fill(Asset.Colors.primary.color)
-                                                    .frame(width: 20, height: 20)
-                                                    .overlay {
-                                                        Circle()
-                                                            .fill(Asset.Colors.secondary.color)
-                                                            .frame(width: 8, height: 8)
+            ScrollViewReader { value in
+                WithPerceptionTracking {
+                    VStack(alignment: .center, spacing: 0) {
+                        Asset.Colors.secondary.color
+                            .frame(height: 1)
+                        
+                        ScrollView {
+                            ForEach(ZcashSDKEnvironment.Servers.allCases, id: \.self) { server in
+                                WithPerceptionTracking {
+                                    VStack {
+                                        HStack(spacing: 0) {
+                                            Button {
+                                                store.send(.someServerTapped(server))
+                                            } label: {
+                                                HStack(spacing: 10) {
+                                                    WithPerceptionTracking {
+                                                        if server == store.server {
+                                                            Circle()
+                                                                .fill(Asset.Colors.primary.color)
+                                                                .frame(width: 20, height: 20)
+                                                                .overlay {
+                                                                    Circle()
+                                                                        .fill(Asset.Colors.secondary.color)
+                                                                        .frame(width: 8, height: 8)
+                                                                }
+                                                        } else {
+                                                            Circle()
+                                                                .stroke(Asset.Colors.primary.color)
+                                                                .frame(width: 20, height: 20)
+                                                        }
                                                     }
-                                            } else {
-                                                Circle()
-                                                    .stroke(Asset.Colors.primary.color)
-                                                    .frame(width: 20, height: 20)
+                                                    
+                                                    Text(server.server()).tag(server)
+                                                        .font(.custom(FontFamily.Inter.medium.name, size: 14))
+                                                        .foregroundColor(Asset.Colors.shade30.color)
+                                                }
+                                            }
+                                            .padding(.vertical, 6)
+                                            
+                                            Spacer()
+                                        }
+                                        .frame(maxWidth: .infinity)
+                                        
+                                        WithPerceptionTracking {
+                                            if store.server == .custom && server == .custom {
+                                                TextField(L10n.ServerSetup.placeholder, text: $store.customServer)
+                                                    .frame(height: 40)
+                                                    .font(.custom(FontFamily.Inter.medium.name, size: 14))
+                                                    .foregroundColor(Asset.Colors.shade30.color)
+                                                    .autocapitalization(.none)
+                                                    .multilineTextAlignment(.leading)
+                                                    .padding(.horizontal, 10)
+                                                    .overlay {
+                                                        Rectangle()
+                                                            .stroke(Asset.Colors.primary.color, lineWidth: 1)
+                                                    }
+                                                    .padding(.top, 8)
                                             }
                                         }
-                                        
-                                        Text(server.server()).tag(server)
-                                            .font(.custom(FontFamily.Inter.medium.name, size: 14))
-                                            .foregroundColor(Asset.Colors.shade30.color)
                                     }
+                                    .padding(.horizontal, 35)
+                                    .id(server == store.server ? InputID.selection : nil)
                                 }
-                                .padding(.vertical, 6)
-                                
-                                Spacer()
                             }
+                            .padding(.vertical, 20)
+                            .task { @MainActor in
+                                try? await Task.sleep(nanoseconds: Constants.delayBeforeScroll)
+                                withAnimation {
+                                    value.scrollTo(InputID.selection, anchor: .center)
+                                }
+                            }
+                        }
+                        
+                        Asset.Colors.shade30.color
                             .frame(maxWidth: .infinity)
-                            
-                            WithPerceptionTracking {
-                                if store.server == .custom && server == .custom {
-                                    TextField(L10n.ServerSetup.placeholder, text: $store.customServer)
-                                        .frame(height: 40)
-                                        .font(.custom(FontFamily.Inter.medium.name, size: 14))
-                                        .foregroundColor(Asset.Colors.shade30.color)
-                                        .autocapitalization(.none)
-                                        .multilineTextAlignment(.leading)
-                                        .padding(.horizontal, 10)
-                                        .overlay {
-                                            Rectangle()
-                                                .stroke(Asset.Colors.primary.color, lineWidth: 1)
-                                        }
-                                        .padding(.top, 8)
+                            .frame(height: 1)
+                            .opacity(0.15)
+                        
+                        WithPerceptionTracking {
+                            Button {
+                                store.send(.setServerTapped)
+                            } label: {
+                                if store.isUpdatingServer {
+                                    HStack(spacing: 8) {
+                                        Text(L10n.General.save.uppercased())
+                                        ProgressView()
+                                    }
+                                } else {
+                                    Text(L10n.General.save.uppercased())
                                 }
                             }
+                            .zcashStyle()
+                            .padding(.top, 20)
+                            .padding(.bottom, 35)
+                            .padding(.horizontal, 70)
+                            .disabled(store.isUpdatingServer)
                         }
-                        .padding(.horizontal, 35)
                     }
-                    .padding(.top, 20)
-                }
-
-                Asset.Colors.shade30.color
                     .frame(maxWidth: .infinity)
-                    .frame(height: 1)
-                    .opacity(0.15)
-
-                Button {
-                    store.send(.setServerTapped)
-                } label: {
-                    if store.isUpdatingServer {
-                        HStack(spacing: 8) {
-                            Text(L10n.General.save.uppercased())
-                            ProgressView()
-                        }
-                    } else {
-                        Text(L10n.General.save.uppercased())
+                    .zashiBack(store.isUpdatingServer)
+                    .zashiTitle {
+                        Text(L10n.ServerSetup.title.uppercased())
+                            .font(.custom(FontFamily.Archivo.bold.name, size: 14))
                     }
+                    .onAppear {
+                        store.send(.onAppear)
+                    }
+                    .alert($store.scope(state: \.alert, action: \.alert))
                 }
-                .zcashStyle()
-                .padding(.top, 20)
-                .padding(.bottom, 35)
-                .padding(.horizontal, 70)
-                .disabled(store.isUpdatingServer)
             }
-            .frame(maxWidth: .infinity)
-            .zashiBack(store.isUpdatingServer)
-            .zashiTitle {
-                Text(L10n.ServerSetup.title.uppercased())
-                    .font(.custom(FontFamily.Archivo.bold.name, size: 14))
-            }
-            .onAppear { store.send(.onAppear) }
-            .alert($store.scope(state: \.alert, action: \.alert))
         }
         .navigationBarTitleDisplayMode(.inline)
     }
