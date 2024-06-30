233fb07b5ef94e7b09ba80247d7e89dadb93370f
Lukas Korba <lukas.korba@seznam.cz>
2024-06-30 08:59:46 +0200

    address-book
    - autofocus on Address Book new record screen added
    - project cleanup

--- modules/Sources/Features/AddressBook/AdressBookNewRecordView.swift
+++ modules/Sources/Features/AddressBook/AdressBookNewRecordView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 import ComposableArchitecture
 import UIComponents
+import Generated
 
 public struct AddressBookNewRecordView: View {
     @Perception.Bindable var store: StoreOf<AddressBook>
     
+    @FocusState public var isAddressFocused: Bool
+    @FocusState public var isNameFocused: Bool
+
     public init(store: StoreOf<AddressBook>) {
         self.store = store
     }
@@ -19,30 +23,54 @@ public struct AddressBookNewRecordView: View {
     public var body: some View {
         WithPerceptionTracking {
             VStack {
-                Spacer()
+                Text("New Address Record".uppercased())
+                    .font(.custom(FontFamily.Archivo.bold.name, size: 14))
+                    .padding(.top, 15)
                 
                 ZashiTextField(
                     text: $store.address,
                     placeholder: "Zcash address",
                     title: "Address:"
                 )
-                .padding(.top, 20)
-                
+                .focused($isAddressFocused)
+                .padding(.top, 40)
+                .submitLabel(.next)
+                .onSubmit {
+                    isNameFocused = true
+                }
+
                 ZashiTextField(
                     text: $store.name,
                     placeholder: "Any name or nickname",
                     title: "Contact name:"
                 )
+                .focused($isNameFocused)
                 .padding(.vertical, 20)
-                
+                .submitLabel(.return)
+                .onSubmit {
+                    if store.isValidForm {
+                        store.send(.saveButtonTapped)
+                    }
+                }
+
                 Button("Save".uppercased()) {
                     store.send(.saveButtonTapped)
                 }
                 .zcashStyle()
-                .padding(.bottom, 20)
+                .padding(.vertical, 20)
+                .padding(.horizontal, 50)
                 .disabled(!store.isValidForm)
+                
+                Spacer()
             }
             .padding(.horizontal, 35)
+            .onAppear {
+                if let id = store.recordToBeAdded?.id, id == "" {
+                    isAddressFocused = true
+                } else {
+                    isNameFocused = true
+                }
+            }
         }
         .applyScreenBackground()
     }
