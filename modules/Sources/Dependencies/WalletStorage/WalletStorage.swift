5955c3a6aabfa1acf524b8054f5e49a99fd9df18
Lukas Korba <lukas.korba@seznam.cz>
2025-02-10 13:09:02 +0100

    [#1458] Release 1.3.3
    - changelog updated

--- modules/Sources/Dependencies/WalletStorage/WalletStorage.swift
+++ modules/Sources/Dependencies/WalletStorage/WalletStorage.swift
@@ -95,7 +95,7 @@ public struct WalletStorage {
         }
         
         guard let wallet = try decode(json: reqData, as: StoredWallet.self) else {
-            throw KeychainError.uninitializedWallet
+            throw WalletStorageError.uninitializedWallet
         }
         
         guard wallet.version == Constants.zcashKeychainVersion else {
