7a91616aff253ad39a508dc66c7ee8e841362c7d
Lukas Korba <lukas.korba@seznam.cz>
2025-03-25 14:55:21 +0100

    Root subflows Batch 4
    - scan with sendings for all possible scenarios finished
    - currency conversion path from home finished
    - transaction details, list and seng again flows finished

--- modules/Sources/Features/CoordFlows/TransactionsCoordFlowCoordinator.swift
+++ modules/Sources/Features/CoordFlows/TransactionsCoordFlowCoordinator.swift
@@ -9,6 +9,7 @@ import ComposableArchitecture
 import Generated
 
 // Path
+import AddressBook
 import TransactionDetails
 import TransactionsManager
 
@@ -16,11 +17,51 @@ extension TransactionsCoordFlow {
     public func coordinatorReduce() -> Reduce<TransactionsCoordFlow.State, TransactionsCoordFlow.Action> {
         Reduce { state, action in
             switch action {
+
+                // MARK: - Address Book Contact
+
+            case .path(.element(id: _, action: .addressBookContact(.dismissAddContactRequired))):
+                let _ = state.path.popLast()
+                return .none
+                
+                // MARK: - Self: Transaction Details
+
+            case .transactionDetails(.saveAddressTapped):
+                var addressBookState = AddressBook.State.initial
+                addressBookState.address = state.transactionDetailsState.transaction.address
+                addressBookState.isValidZcashAddress = true
+                addressBookState.isNameFocused = true
+                state.path.append(.addressBookContact(addressBookState))
+                return .none
+                
+                // MARK: - Self: Transaction Manager
+                
+            case .transactionsManager(.transactionTapped(let txId)):
+                if let index = state.transactions.index(id: txId) {
+                    var transactionDetailsState = TransactionDetails.State.initial
+                    transactionDetailsState.transaction = state.transactions[index]
+                    state.path.append(.transactionDetails(transactionDetailsState))
+                }
+                return .none
                 
                 // MARK: - Transaction Details
                 
+            case .path(.element(id: _, action: .transactionDetails(.saveAddressTapped))):
+                for element in state.path {
+                    if case .transactionDetails(let transactionDetailsState) = element {
+                        var addressBookState = AddressBook.State.initial
+                        addressBookState.address = transactionDetailsState.transaction.address
+                        addressBookState.isValidZcashAddress = true
+                        addressBookState.isNameFocused = true
+                        state.path.append(.addressBookContact(addressBookState))
+                    }
+                }
+                return .none
                 
-                
+            case .path(.element(id: _, action: .transactionDetails(.closeDetailTapped))):
+                let _ = state.path.removeLast()
+                return .none
+
             default: return .none
             }
         }
